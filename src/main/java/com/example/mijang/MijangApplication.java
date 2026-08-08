b12b05b944c7ecbc922d57c1d0a1bbff1d0af95f
jeonggul <dlwjdgkw@gmail.com>
2026-08-09 04:44:05 +0900

    Initial commit

---
+++ src/main/java/com/example/mijang/MijangApplication.java
@@ -0,0 +1,13 @@
+package com.example.mijang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MijangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MijangApplication.class, args);
+	}
+
+}
